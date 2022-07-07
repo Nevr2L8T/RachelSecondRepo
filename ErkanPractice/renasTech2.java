0bee5c2d3742753d78f98b8370f4b8328ca3c99a
Nevr2L8T <108486568+Nevr2L8T@users.noreply.github.com>
2022-07-07 09:23:26 -0500

    This is my message1

---
+++ ErkanPractice/renasTech2.java
@@ -0,0 +1,7 @@
+package ErkanPractice;
+
+public class renasTech2 {
+    public static void main(String[] args) {
+        System.out.println("username for renastech is = renastech");
+    }
+}
---
+++ gitIgnorePakage/practiceGit.java
@@ -0,0 +1,7 @@
+package gitIgnorePakage;
+
+public class practiceGit {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
